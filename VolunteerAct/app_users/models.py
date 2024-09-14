54b35c039e1ac28b2e18cf32761a61fbe16dd370
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-09-14 15:02:57 +0300

    is_staff field added

--- VolunteerAct/app_users/models.py
+++ VolunteerAct/app_users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
 from VolunteerAct.app_users.managers import AppUserManager
+from django.utils.translation import gettext_lazy as _
 
 
 class AppUser(AbstractBaseUser, PermissionsMixin):
@@ -10,6 +11,12 @@ class AppUser(AbstractBaseUser, PermissionsMixin):
         unique=True,
         null=False,
         blank=False
+    )
+
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
     )
 
     USERNAME_FIELD = 'email'
