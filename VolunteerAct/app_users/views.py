73f93ab8db13560b9acdc56037395be05c4b35bc
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-09-16 00:25:49 +0300

    Register path

--- VolunteerAct/app_users/views.py
+++ VolunteerAct/app_users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+from VolunteerAct.app_users.forms import AppUserForm
+
+
+class RegisterUserView(CreateView):
+    template_name = 'app_users/register_page.html'
+    form_class = AppUserForm
