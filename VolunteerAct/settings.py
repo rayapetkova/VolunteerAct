847c9e5e5165c5c5c589be2cdf16ce7f9db9d4a3
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-09-16 00:03:51 +0300

    Static files setup

--- VolunteerAct/settings.py
+++ VolunteerAct/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 
 from decouple import config
@@ -113,6 +114,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
