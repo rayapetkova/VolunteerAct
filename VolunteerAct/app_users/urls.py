817be802ae6fc2805c58c15298d51ee9c36e1b7d
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-09-19 22:42:33 +0300

    Update view - profile details edit

--- VolunteerAct/app_users/urls.py
+++ VolunteerAct/app_users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from VolunteerAct.app_users.views import RegisterUserView, LoginUserView
+from VolunteerAct.app_users.views import RegisterUserView, LoginUserView, ProfileUpdateView
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name="register-user"),
-    path('login/', LoginUserView.as_view(), name="login-user")
+    path('login/', LoginUserView.as_view(), name="login-user"),
+    path('<int:pk>/edit_profile', ProfileUpdateView.as_view(), name="edit-profile")
 ]
--- VolunteerAct/app_users/views.py
+++ VolunteerAct/app_users/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, UpdateView
 
 from VolunteerAct.app_users.forms import AppUserForm
+from VolunteerAct.app_users.models import Profile
 
 
 class RegisterUserView(CreateView):
@@ -21,3 +22,14 @@ class RegisterUserView(CreateView):
 class LoginUserView(LoginView):
     template_name = 'app_users/login_page.html'
     success_url = reverse_lazy('home-page')
+
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    template_name = 'app_users/edit_profile.html'
+    fields = ('first_name', 'last_name', 'phone_number', 'bio')
+
+    def get_success_url(self):
+        return reverse('edit-profile', kwargs={
+            "pk": self.request.user.profile.id
+        })
