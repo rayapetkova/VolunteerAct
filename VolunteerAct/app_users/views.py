a01d84b320ca2915ebb8667d10aa30b4e042cb4b
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-09-19 21:52:59 +0300

    Return to home page after successful register or login

--- VolunteerAct/app_users/views.py
+++ VolunteerAct/app_users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from VolunteerAct.app_users.forms import AppUserForm
@@ -9,6 +10,7 @@ from VolunteerAct.app_users.forms import AppUserForm
 class RegisterUserView(CreateView):
     template_name = 'app_users/register_page.html'
     form_class = AppUserForm
+    success_url = reverse_lazy('home-page')
 
     def form_valid(self, form):
         result = super().form_valid(form)
@@ -18,3 +20,4 @@ class RegisterUserView(CreateView):
 
 class LoginUserView(LoginView):
     template_name = 'app_users/login_page.html'
+    success_url = reverse_lazy('home-page')
