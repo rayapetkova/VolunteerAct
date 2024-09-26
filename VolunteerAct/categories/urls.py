1c8587c54e10e4562429c43e9eb9b91fbee82a83
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-09-26 15:06:46 +0300

    Detail view for event

--- VolunteerAct/categories/urls.py
+++ VolunteerAct/categories/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from VolunteerAct.categories.views import CategoryDetailsView
+from VolunteerAct.categories.views import CategoryDetailsView, EventDetailsView
 
 urlpatterns = [
-    path('<int:pk>/', CategoryDetailsView.as_view(), name='category-page')
+    path('<int:pk>/', CategoryDetailsView.as_view(), name='category-page'),
+    path('<int:categoryId>/events/<int:pk>', EventDetailsView.as_view(), name='event-page')
 ]
--- VolunteerAct/categories/views.py
+++ VolunteerAct/categories/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 
-from VolunteerAct.categories.models import Category
+from VolunteerAct.categories.models import Category, Event
 
 
 class CategoryDetailsView(DetailView):
     model = Category
     template_name = 'categories/category_page.html'
+
+
+class EventDetailsView(DetailView):
+    model = Event
+    template_name = 'categories/event_page.html'
