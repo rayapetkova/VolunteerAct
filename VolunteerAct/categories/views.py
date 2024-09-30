c83c2315bbe465b72a3d762ffd06daea5a643a76
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-09-30 17:01:14 +0300

    Display upcoming and past events

--- VolunteerAct/categories/views.py
+++ VolunteerAct/categories/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.views.generic import DetailView
 
 from VolunteerAct.categories.models import Category, Event
@@ -7,6 +8,17 @@ from VolunteerAct.categories.models import Category, Event
 class CategoryDetailsView(DetailView):
     model = Category
     template_name = 'categories/category_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        upcoming_events = self.object.category_events.filter(time__gt=timezone.now())
+        past_events = self.object.category_events.filter(time__lt=timezone.now())
+
+        context['upcoming_events'] = upcoming_events
+        context['past_events'] = past_events
+
+        return context
 
 
 class EventDetailsView(DetailView):
