6c27e3e092e92df46c51385e73fb90295d76c41a
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-10-12 21:40:36 +0300

    Register AppUser and Profile models in admin

--- VolunteerAct/app_users/admin.py
+++ VolunteerAct/app_users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from VolunteerAct.app_users.models import AppUser, Profile
+
+
+@admin.register(AppUser)
+class AppUserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
