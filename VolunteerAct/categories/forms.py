e0079ccb279de8701bccf8c629cc672ee02f644d
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-10-17 23:23:45 +0300

    Filter form

--- VolunteerAct/categories/forms.py
+++ VolunteerAct/categories/forms.py
@@ -1,6 +1,23 @@
-from django.forms import ModelForm, widgets
+from django import forms
+from django.db.models import Count
+from django.forms import ModelForm, widgets, Form
+from django.utils import timezone
 
-from VolunteerAct.categories.models import Event
+from VolunteerAct.categories.models import Event, Category
+
+
+def get_categories():
+    categories = Category.objects.all()
+    categories_names_tuple = [(category.name, category.name) for category in categories]
+
+    return categories_names_tuple
+
+
+def get_cities():
+    cities = Event.objects.values('city').annotate(cities_count=Count('city')).order_by('-cities_count')
+    cities_tuple = [(cityDict['city'], f"{cityDict['city']} ({cityDict['cities_count']})") for cityDict in cities]
+
+    return cities_tuple
 
 
 class EventForm(ModelForm):
@@ -20,3 +37,20 @@ class EventForm(ModelForm):
                 attrs={'type': 'datetime-local'}
             )
         }
+
+
+class FilterForm(Form):
+    CATEGORY_CHOICES = get_categories()
+    CITY_CHOICES = get_cities()
+
+    category = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+        choices=CATEGORY_CHOICES
+    )
+
+    city = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+        choices=CITY_CHOICES
+    )
