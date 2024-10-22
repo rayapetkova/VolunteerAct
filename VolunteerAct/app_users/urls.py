62b68b12ab317e2ebd931a99c4a3774095d40ffe
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-10-22 23:00:14 +0300

    Change URLs

--- VolunteerAct/app_users/urls.py
+++ VolunteerAct/app_users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from VolunteerAct.app_users.views import RegisterUserView, LoginUserView, ProfileUpdateView, logout_view
+from VolunteerAct.app_users.views import RegisterUserView, LoginUserView, ProfileView, logout_view
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name="register-user"),
     path('login/', LoginUserView.as_view(), name="login-user"),
-    path('edit_profile/', ProfileUpdateView.as_view(), name="edit-profile"),
-    path('logout/', logout_view, name="logout-user")
+    path('logout/', logout_view, name="logout-user"),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile-details-update')
 ]
