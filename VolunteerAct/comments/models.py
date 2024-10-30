b64b66aadca6e698a30c0fe33f5daf626ced975d
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-10-30 19:10:42 +0200

    Comment model

--- VolunteerAct/comments/models.py
+++ VolunteerAct/comments/models.py
@@ -1,3 +1,21 @@
+from django.core import validators
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    body = models.TextField(
+        null=False,
+        blank=False,
+        max_length=500,
+        validators=[
+            validators.MinLengthValidator(10, message='Comment needs to be at least 10 characters.')
+        ]
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
