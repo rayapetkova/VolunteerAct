e2abdcff04d73c3b237d6365719576708e551c4f
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-11-02 20:44:39 +0200

    Favourites model

--- VolunteerAct/favourites/models.py
+++ VolunteerAct/favourites/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from VolunteerAct.categories.models import Event
+
+AppUserModel = get_user_model()
+
+
+class Favourites(models.Model):
+    user = models.ForeignKey(
+        to=AppUserModel,
+        on_delete=models.CASCADE,
+        related_name='user_favourites'
+    )
+
+    event = models.ForeignKey(
+        to=Event,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        unique_together = ('user', 'event')
