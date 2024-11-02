ee17a9021a4e532be7104968b3481b5d8b0d06d0
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-11-02 21:35:46 +0200

    Add event to favourites

--- VolunteerAct/favourites/urls.py
+++ VolunteerAct/favourites/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from VolunteerAct.favourites.views import FavouritesCreateApiView
 
 urlpatterns = [
-    path('', FavouritesCreateApiView.as_view(), name='list-create-favourites-record')
+    path('', FavouritesCreateApiView.as_view(), name='list-create-favourites-record-api')
 ]
