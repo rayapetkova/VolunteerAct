193a53918a8305fde719d9500a86027da8dbb9e9
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-11-04 19:22:33 +0200

    Comment details api view

--- VolunteerAct/comments/urls.py
+++ VolunteerAct/comments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from VolunteerAct.comments.views import CommentListApiView
+from VolunteerAct.comments.views import CommentListApiView, CommentDetailsApiView
 
 urlpatterns = [
-    path('', CommentListApiView.as_view(), name='list-create-comments-api')
+    path('', CommentListApiView.as_view(), name='list-create-comments-api'),
+    path('<int:pk>/', CommentDetailsApiView.as_view(), name='details-comment-api')
 ]
--- VolunteerAct/comments/views.py
+++ VolunteerAct/comments/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import status
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -24,3 +25,10 @@ class CommentListApiView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetailsApiView(RetrieveAPIView):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        return Comment.objects.all()
