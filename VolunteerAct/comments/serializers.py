3a66c8a6ab782283ebc59676ee83656082f23a77
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-11-04 19:23:07 +0200

    Return image url - serializer method

--- VolunteerAct/comments/serializers.py
+++ VolunteerAct/comments/serializers.py
@@ -1,3 +1,4 @@
+from cloudinary.utils import cloudinary_url
 from rest_framework import serializers
 
 from VolunteerAct.app_users.serializers import AppUserSerializer
@@ -6,10 +7,17 @@ from VolunteerAct.comments.models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
     user = AppUserSerializer(many=False)
+    profile_image_full_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ('profile_image_full_url', 'body', 'created_at', 'updated_at', 'event', 'user')
+
+    def get_profile_image_full_url(self, obj):
+        if obj.user.profile.profile_image:
+            return obj.user.profile.profile_image.url
+
+        return None
 
 
 class CommentCreateSerializer(serializers.ModelSerializer):
