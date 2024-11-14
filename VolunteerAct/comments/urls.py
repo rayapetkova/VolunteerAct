256d6492e8aad069c021f40b102ccfa3722a1fd5
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-11-14 23:52:50 +0200

    Edit and delete comment view and url

--- VolunteerAct/comments/urls.py
+++ VolunteerAct/comments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from VolunteerAct.comments.views import CommentListApiView, CommentDetailsApiView
+from VolunteerAct.comments.views import CommentListApiView, CommentDetailsApiView, CommentEditAndDeleteApiView
 
 urlpatterns = [
     path('', CommentListApiView.as_view(), name='list-create-comments-api'),
-    path('<int:pk>/', CommentDetailsApiView.as_view(), name='details-comment-api')
+    path('<int:pk>/', CommentDetailsApiView.as_view(), name='details-comment-api'),
+    path('<int:pk>/edit_delete', CommentEditAndDeleteApiView.as_view(), name='edit-delete-comment-api')
 ]
--- VolunteerAct/comments/views.py
+++ VolunteerAct/comments/views.py
@@ -32,3 +32,16 @@ class CommentDetailsApiView(RetrieveAPIView):
 
     def get_queryset(self):
         return Comment.objects.all()
+
+
+class CommentEditAndDeleteApiView(APIView):
+
+    def patch(self, request, pk):
+        comment = Comment.objects.all().filter(id=pk).first()
+        serializer = CommentSerializer(comment, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
