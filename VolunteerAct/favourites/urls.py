8ea00b51b4a36d66959c2f54fb814f488f9b862b
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-11-16 00:42:30 +0200

    Favourite events - view and url

--- VolunteerAct/favourites/urls.py
+++ VolunteerAct/favourites/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from VolunteerAct.favourites.views import FavouritesCreateApiView
+from VolunteerAct.favourites.views import FavouritesCreateApiView, favourite_events_view
 
 urlpatterns = [
-    path('', FavouritesCreateApiView.as_view(), name='list-create-favourites-record-api')
+    path('', favourite_events_view, name='user-favourite-events-page'),
+    path('api/', FavouritesCreateApiView.as_view(), name='list-create-favourites-record-api')
 ]
--- VolunteerAct/favourites/views.py
+++ VolunteerAct/favourites/views.py
@@ -1,9 +1,47 @@
 from django.shortcuts import render
+from django.utils import timezone
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from VolunteerAct.categories.forms import FilterForm
+from VolunteerAct.favourites.models import Favourites
 from VolunteerAct.favourites.serializers import FavouritesSerializer
+
+
+def favourite_events_view(request):
+    filter_form = FilterForm(request.GET or None)
+
+    user_favourite_events = Favourites.objects.all().filter(user=request.user).order_by('-added_on')
+
+    for favourite_event in user_favourite_events:
+        favourite_event.already_passed = True if favourite_event.event.time < timezone.now() else False
+
+    if request.method == "GET":
+        if filter_form.is_valid():
+            categories_filter = request.GET.getlist('category')
+            cities_filter = request.GET.getlist('city')
+            time_filter = request.GET.getlist('time')
+
+            if categories_filter:
+                user_favourite_events = user_favourite_events.filter(event__category__name__in=categories_filter)
+
+            if cities_filter:
+                user_favourite_events = user_favourite_events.filter(event__city__in=cities_filter)
+
+            if len(time_filter) == 1:
+                if 'Past' in time_filter:
+                    user_favourite_events = user_favourite_events.filter(event__time__lt=timezone.now())
+
+                if 'Upcoming' in time_filter:
+                    user_favourite_events = user_favourite_events.filter(event__time__gte=timezone.now())
+
+    context = {
+        'filter_form': filter_form,
+        'user_favourite_events': user_favourite_events
+    }
+
+    return render(request, 'favourites/favourite_events.html', context=context)
 
 
 class FavouritesCreateApiView(APIView):
