79aa185026a345fb7ba2c54b56cabfa32525b16d
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-11-19 00:22:39 +0200

    Check if user is authenticated

--- VolunteerAct/home/views.py
+++ VolunteerAct/home/views.py
@@ -15,13 +15,14 @@ def home_page(request):
     pixabay_api_url = config('PIXABAY_API_URL')
     upcoming_events = Event.objects.filter(time__gte=timezone.now()).order_by('time')[:4]
 
-    for event in upcoming_events:
-        saved_to_favourites_by_logged_in_user = Favourites.objects.all().filter(user=request.user, event=event).first()
+    if request.user.is_authenticated:
+        for event in upcoming_events:
+            saved_to_favourites_by_logged_in_user = Favourites.objects.all().filter(user=request.user, event=event).first()
 
-        if saved_to_favourites_by_logged_in_user:
-            event.saved_to_favourites_by_logged_in_user = True
-        else:
-            event.saved_to_favourites_by_logged_in_user = False
+            if saved_to_favourites_by_logged_in_user:
+                event.saved_to_favourites_by_logged_in_user = True
+            else:
+                event.saved_to_favourites_by_logged_in_user = False
 
     context = {
         'categories': Category.objects.order_by('id').all(),
