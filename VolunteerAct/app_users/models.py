8b2690b88d13fbcbf9dc40019db7fd3a2670f3bd
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-11-19 00:26:08 +0200

    Change error message for unique for email field

--- VolunteerAct/app_users/models.py
+++ VolunteerAct/app_users/models.py
@@ -14,7 +14,10 @@ class AppUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(
         unique=True,
         null=False,
-        blank=False
+        blank=False,
+        error_messages={
+            'unique': "User with this email already exists."
+        }
     )
 
     is_staff = models.BooleanField(
