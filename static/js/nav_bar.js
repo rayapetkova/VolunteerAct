49f972c3f9326c13d3648272d7549b5540fce02f
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-11-19 20:37:54 +0200

    Dark login link in nav bar

--- static/js/nav_bar.js
+++ static/js/nav_bar.js
@@ -4,6 +4,9 @@ const darkLogo = document.currentScript.getAttribute('darkLogo')
 const headerElement = document.getElementsByTagName('header')[0]
 const imgLogoAElement = document.getElementsByClassName('img-logo')[0]
 const imageLogo = imgLogoAElement.getElementsByTagName('img')[0]
+
+const signInAElement = document.querySelector('.sign-in-li a')
+
 const favouritesLiElement = document.getElementsByClassName('favourite-events')[0]
 const favouritesAElement = favouritesLiElement.getElementsByTagName('a')[0]
 const ticketsLiElement = document.getElementsByClassName('tickets-events')[0]
@@ -15,10 +18,12 @@ document.addEventListener('scroll', () => {
         imageLogo.src = lightLogo
         favouritesAElement.style.color = '#FFFFFFFF'
         ticketsAElement.style.color = '#FFFFFFFF'
+        signInAElement.style.color = '#FFFFFFFF'
     } else {
         headerElement.style.backgroundColor = 'transparent'
         imageLogo.src = darkLogo
         favouritesAElement.style.color = '#203069'
         ticketsAElement.style.color = '#203069'
+        signInAElement.style.color = '#203069'
     }
 })
