7b0bf359afb8bbcc94fc0c6af965c8ac62796997
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-11-21 22:36:34 +0200

    Email configurations

--- VolunteerAct/settings.py
+++ VolunteerAct/settings.py
@@ -141,3 +141,11 @@ cloudinary.config(
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = True
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
