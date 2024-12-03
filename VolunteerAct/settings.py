8788267c49ddd6ef5314affabb549052e03bac98
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-12-04 01:15:25 +0200

    Whitenoise setup

--- VolunteerAct/settings.py
+++ VolunteerAct/settings.py
@@ -58,7 +58,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'allauth.account.middleware.AccountMiddleware'
+    'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'VolunteerAct.urls'
