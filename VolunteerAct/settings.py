d71a87413e251fdc9f17b1f99d63c7d81dabf290
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-12-04 00:20:56 +0200

    Update settings and requirements

--- VolunteerAct/settings.py
+++ VolunteerAct/settings.py
@@ -84,16 +84,28 @@ WSGI_APPLICATION = 'VolunteerAct.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config('DB_NAME'),
-        "USER": config('DB_USER'),
-        "PASSWORD": config('DB_PASSWORD'),
-        "HOST": config('DB_HOST'),
-        "PORT": config('DB_PORT'),
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": 'volunteer_act_db',
+            "USER": 'postgres',
+            "PASSWORD": 'postgres',
+            "HOST": 'localhost',
+            "PORT": '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": config('DB_NAME'),
+            "USER": config('DB_USER'),
+            "PASSWORD": config('DB_PASSWORD'),
+            "HOST": config('DB_HOST'),
+            "PORT": config('DB_PORT'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
