42b9ef631d2018e3455eb54e67c88d1c1404315f
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-12-08 20:33:02 +0200

    Correct validators

--- VolunteerAct/app_users/validators.py
+++ VolunteerAct/app_users/validators.py
@@ -2,21 +2,21 @@ from django.core.exceptions import ValidationError
 
 
 def first_name_validator(value):
-    if not value.is_alpha():
+    if not value.isalpha():
         raise ValidationError('First name needs to contain only alphabetic symbols.')
 
     return value
 
 
 def last_name_validator(value):
-    if not value.is_alpha():
+    if not value.isalpha():
         raise ValidationError('Last name needs to contain only alphabetic symbols.')
 
     return value
 
 
 def phone_number_validator(value):
-    if not value.is_digit():
+    if not value.isdigit():
         raise ValidationError('Phone number needs to consist of only digits.')
 
     return value
